12fcb250de423309f093de37c21f54e310078d01
Kyle McAlpine <k@kylejm.io>
2016-05-16 12:44:27 +0100

    Adding script for correcting git commit and author emails

--- Dotfiles/.bash_profile
+++ Dotfiles/.bash_profile
@@ -1,3 +1,3 @@
-PATH="/usr/local/git/bin:$HOME/.rbenv/bin:$PATH"
+PATH="/usr/local/git/bin:$HOME/bin:$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 if which swiftenv > /dev/null; then eval "$(swiftenv init -)"; fi
--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -4,5 +4,7 @@ echo 'installing dotfiles'
 
 for dotfile in $(find -H Dotfiles -mindepth 1 -maxdepth 1)
 do
-    ln -s "$dotfile" "$HOME/$src"
+    ln -s "$dotfile" "$HOME/$dotfile"
 done
+
+ln -s $(pwd)/bin $HOME/bin
