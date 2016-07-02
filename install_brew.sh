00d0f598ecbbb456260047039aed10a53e86ca6a
Kyle McAlpine <k@kylejm.io>
2016-07-02 21:34:30 +0100

    corrected brew url

--- install_brew.sh
+++ install_brew.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-BREW_URL="https://raw.github.com/mxcl/homebrew/go/install"
+BREW_URL="https://raw.githubusercontent.com/Homebrew/install/master/install"
 if [ ! -x "/usr/local/bin/brew" ]; then
     curl -fsSL "${BREW_URL}" > brew_install.rb
     ruby ./brew_install.rb
