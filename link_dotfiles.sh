0fca43329c513558de913fa4c4fc9faf3968fa83
Kyle McAlpine <k@kylejm.io>
2016-08-09 15:35:36 +0100

    Fixing link_dotfiles.sh

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-
-echo 'installing dotfiles'
-
-for dotfile in $(find -H Dotfiles -mindepth 1 -maxdepth 1)
-do
-    ln -s "$dotfile" "$HOME/$dotfile"
+DOTFILES_DIR="Dotfiles"
+PWD=$(pwd)
+for FILE_PATH in $(find $DOTFILES_DIR -mindepth 1 -maxdepth 1)
+  do
+    FILE=${FILE_PATH#$DOTFILES_DIR/}
+    HOME_PATH=$HOME/$FILE
+    FULL_PATH=$PWD/$FILE_PATH
+    rm -r $HOME_PATH
+    ln -s $FULL_PATH $HOME_PATH
 done
-
-ln -s $(pwd)/bin $HOME/bin
