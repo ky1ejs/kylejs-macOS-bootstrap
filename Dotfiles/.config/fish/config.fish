1b9ff190e4470a67d850e0bafa7efd3ae1829aa8
Kyle McAlpine <k@kylejm.io>
2017-09-29 17:46:33 +0100

    Updating fish and gocode config

--- Dotfiles/.config/fish/config.fish
+++ Dotfiles/.config/fish/config.fish
@@ -7,7 +7,7 @@ set -gx OMF_PATH "/Users/kylejm/.local/share/omf"
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
-set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_121.jdk/Contents/Home"
+set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home"
 
 set PATH $HOME/.rbenv/shims $HOME/bin $JAVA_HOME/bin $PATH
 rbenv init - | source
