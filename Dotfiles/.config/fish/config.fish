9cb3a24f55692ec395d0c54df1e797273cc5bca4
Kyle McAlpine <kyle@kjm.bio>
2019-01-21 20:05:05 +0000

    Fixes for OMF

--- Dotfiles/.config/fish/config.fish
+++ Dotfiles/.config/fish/config.fish
@@ -1,11 +1,2 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH "/Users/kylejm/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/Users/kylejm/.config/omf"
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
 set PATH $HOME/.rbenv/shims $HOME/bin $PATH
 rbenv init - | source
