5c828b37f388239553ccc8eb649f120a0e09c3cc
Kyle McAlpine <kyle@kjm.bio>
2019-02-13 18:24:01 +0000

    Only setting theme if dir existts

--- link_xcode_theme.sh
+++ link_xcode_theme.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-ln -s $PWD/kylejm.xccolortheme $HOME/Library/Developer/Xcode/UserData/FontAndColorThemes/
+source functions.sh
+
+THEME_LOCATION = $HOME/Library/Developer/Xcode/UserData/FontAndColorThemes/
+
+if [ ! -d THEME_LOCATION ]; then
+	printMessage "Xcode not installed, please install to set theme"
+	exit 0
+fi
+
+ln -s $PWD/kylejm.xccolortheme $THEME_LOCATION
