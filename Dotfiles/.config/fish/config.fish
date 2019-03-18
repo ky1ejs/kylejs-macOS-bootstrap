752be72e7de4fc3bd15682ec0e196212730bb716
Kyle McAlpine <kyle@kjm.bio>
2019-03-18 10:22:35 +0000

    Adding fish aliases for git

--- Dotfiles/.config/fish/config.fish
+++ Dotfiles/.config/fish/config.fish
@@ -1,2 +1,15 @@
 set PATH $HOME/.rbenv/shims $HOME/bin $PATH
 rbenv init - | source
+
+# Aliases
+alias gs='git status'
+alias gd='git diff'
+alias ga='git add'
+alias gpl='git pull'
+alias gps='git push'
+alias gpsu='git push -u origin HEAD'
+alias gcam='git commit -a -m'
+alias gco='git checkout'
+alias gcob='git checkout -b'
+alias gr!='git reset --hard HEAD'
+
