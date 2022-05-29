6e79681177d8acb2c71554ee11b005dba11419ad
Kyle Satti <k@kylejs.dev>
2022-05-29 19:35:28 +0100

    Remove omf

--- install_fish.sh
+++ install_fish.sh
@@ -10,7 +10,5 @@ fi
 printMessage "Installing fish"
 brew install fish
 
-curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
-
 printMessage "fish installed\n\nTo set as default, run:\n\n\$ sudo echo /usr/local/bin/fish >> /etc/shells
 \n\$ chsh -s `which fish`\n\nFor theming run\n\n\$"
